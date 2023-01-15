035ecdd2c5dc1604c978b26fab3bd6bb191d3d36
SemiCollin <ckwan26.email@gmail.com>
2023-01-15 12:25:44 -0500

    server.py adjustments for fuzzysearch

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,18 @@
 from flask import Flask , request
 from flask_cors import CORS
+import FuzzySearch
 import json
 
 app = Flask(__name__)
 CORS(app)
 
-f_loblaws = open('loblas.json')
-f_no_frills = open('no-frills.json')
-f_fortinos = open('fortinos.json')
+# f_loblaws = open('../loblaws.json')
+# f_no_frills = open('../no-frills.json')
+# f_fortinos = open('../fortinos.json')
 
-data_loblaws = json.load(f_loblaws)
-data_no_frills = json.load(f_no_frills)
-data_fortinos = json.load(f_fortinos)
-
-@app.route('/breakdown' , methods=['GET'])
-def store_breakdown():
-    # get price breakdown from each store to output onto
-    return
-
-@app.route('/grocery-list' , methods=['POST'])
-# POST --> data from frontend to backend
+# data_loblaws = json.load(f_loblaws)
+# data_no_frills = json.load(f_no_frills)
+# data_fortinos = json.load(f_fortinos)
 
 def store_extract(user_list , store_list):
     # extract list of dictionaries for available items
@@ -31,9 +24,27 @@ def store_extract(user_list , store_list):
                 available_items.append(items)
     return available_items
 
-def get_list(user_list):
-    
-    return
+@app.route('/breakdown' , methods=['GET'])
+def store_breakdown():
+
+    return("testing")
+
+@app.route('/grocery-list' , methods=['POST'])
+# POST --> data from frontend to backend
+def grocery_list():
+    # input = request.form['text']
+    # print(input)
+    # l_available = store_extract(input , data_loblaws)
+    # n_available = store_extract(input , data_no_frills)
+    # f_available = store_extract(input , data_fortinos)
+
+    # stores_extracted = [
+    #     l_available, 
+    #     n_available, 
+    #     f_available
+    # ]
+
+    return("testing")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,25 @@
-import React, { useState } from "react";
-import logo from './logo2.jpg'
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const testData = [
-    'test1', 'test2', 'test3'
-  ]
-
-  const handleDivClick = (event, value) => {
-    console.log(value);
-    setBigDiv(<div style={{
-    }}>
-      This is a test div
-    </div>)
-
-  }
-
-  const handleClick = (event) => {
-    console.log(textValue);
-    // DO API CALL HERE
-    // fetch
-    // .then((data) => {
-      const tempArray = [];
-      testData.forEach((value) => {
-        tempArray.push(
-        <div key={value} onClick={event => handleDivClick(event, value)}>
-          {value}
-        </div>)
-      })
-      setTestDivs(tempArray);
-    // })
-  }
-
-  const handleChange = (event) => {
-    setTextValue(event.target.value);
-  }
-
-  const [textValue, setTextValue] = useState('Write grocery list here');
-  const [testDivs, setTestDivs] = useState([]);
-  const [bigDiv, setBigDiv] = useState('');
-
   return (
     <div className="App">
-      <div>
-        <img
-          src={require("./logo2.jpg")}
-          style={{
-          position: "absolute",
-          width: "250px",
-          height: "125px",
-          top: "5px",
-           left: "20px",
-           }}
-         />
-      <div>     
-        
-                        <textarea id = "textarea"
-                          value={textValue}
-                          onChange={handleChange}
-                          style={{
-                          position: "absolute",
-                          left: "20px",
-                          top: "150px",
-                          height: "200px",
-                          width: "290px",
-                          resize: "none",
-                          }}> Write grocery list here
-                        </textarea>
-                        <button onClick={handleClick} style={{position: "absolute",
-                          left: "255px",
-                          top: "360px",
-                          height: "30px",
-                          width: "60px",}}>Submit</button>
-                          <div style={
-                            {
-                              position: "absolute",
-                              // padding: "200px"
-                            }
-                          }>
-                          </div>
-                          {testDivs}
-                          {bigDiv}
-
-                        
-      </div>
-            </div>
-              <iframe style = {{float: "right", width: "1100px", height: "745px"}}src="https://my.atlistmaps.com/map/29e4bf78-1973-4f74-98ec-20c1d91e85b7?share=true" allow="geolocation" width="100%" height="400px" frameborder="0" scrolling="no" allowfullscreen></iframe>
-            </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-
 export default App;
-
-
